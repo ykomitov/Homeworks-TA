58fe38d53fce2fde52e4a826e1576b2230ef4fdb
ykomitov <yavork@gmail.com>
2015-05-28 19:46:30 +0300

    minor errors corrected

--- 06.JavaScriptFundamentals/05.Arrays/scripts/02.Lex.comparison.js
+++ 06.JavaScriptFundamentals/05.Arrays/scripts/02.Lex.comparison.js
@@ -2,16 +2,19 @@ var pr2 = function () {
 
     var arr1,
         arr2,
-        counter = 0;
+        counter = 0,
+        i,
+        min_arr_len;
 
     arr1 = document.getElementById('problem21').value.split('');
     arr2 = document.getElementById('problem22').value.split('');
+    min_arr_len = Math.min(arr1.length, arr2.length);
 
     jsConsole.writeLine('<br>=================== Problem 2 ===================');
     jsConsole.writeLine('array 1: ' + arr1.join(', '));
     jsConsole.writeLine('array 2: ' + arr2.join(', '));
 
-    for (var i = 0; i < Math.min(arr1.length, arr2.length); i += 1) {
+    for (i = 0; i < min_arr_len; i += 1) {
         if (arr1[i] < arr2[i]) {
             console.log('arr1 is lexicographically before arr2');
             jsConsole.writeLine('arr1 is lexicographically before arr2');
@@ -24,7 +27,7 @@ var pr2 = function () {
         }
         else {
             counter += 1;
-            if (counter === Math.min(arr1.length, arr2.length)) {
+            if (counter === min_arr_len) {
                 if (arr1.length < arr2.length) {
                     console.log('arr1 is lexicographically before arr2');
                     jsConsole.writeLine('arr1 is lexicographically before arr2');
