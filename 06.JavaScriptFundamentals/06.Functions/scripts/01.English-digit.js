f94b5950f9253b9a2b1e7ab460cb37eebb03539d
ykomitov <yavork@gmail.com>
2015-06-03 14:24:32 +0300

    HW6 - PR1 & 2 - OK

--- 06.JavaScriptFundamentals/06.Functions/scripts/01.English-digit.js
+++ 06.JavaScriptFundamentals/06.Functions/scripts/01.English-digit.js
@@ -1,21 +1,59 @@
 var pr1 = function () {
 
-    var arr1,
-        i,
-        output;
-
-    arr1 = [];
-
-    for (i = 0; i < 20; i += 1) {
-        arr1[i] = i * 5;
-    }
-
-    output = arr1.join(', ');
+    var txtInput = document.getElementById('problem1').value;
 
     jsConsole.writeLine('<br>=================== Problem 1 ===================');
-    console.log('Array length: ' + arr1.length);
-    console.log('Array: ' + output);
-    jsConsole.writeLine('Array length: ' + arr1.length);
-    jsConsole.writeLine('Array: ' + output);
+    console.log('Input: ' + txtInput);
+    console.log('Last digit: ' + digitAsWord(txtInput));
+    jsConsole.writeLine('Input: ' + txtInput);
+    jsConsole.writeLine('Last digit: ' + digitAsWord(txtInput));
     jsConsole.writeLine('=================================================');
+}
+
+function digitAsWord(input) {
+    var num,
+        output = '';
+
+    if (!(isNaN(input))) {
+        num = (input * 1) | 0;
+
+        switch (num % 10) {
+            case 0:
+                output += 'zero';
+                break;
+            case 1:
+                output += 'one';
+                break;
+            case 2:
+                output += 'two';
+                break;
+            case 3:
+                output += 'three';
+                break;
+            case 4:
+                output += 'four';
+                break;
+            case 5:
+                output += 'five';
+                break;
+            case 6:
+                output += 'six';
+                break;
+            case 7:
+                output += 'seven';
+                break;
+            case 8:
+                output += 'eight';
+                break;
+            case 9:
+                output += 'nine';
+                break;
+            default:
+                break;
+        }
+    }
+    else {
+        output = 'Not a number. Please input a number!';
+    }
+    return output;
 }
--- 06.JavaScriptFundamentals/06.Functions/scripts/02.Reverse-number.js
+++ 06.JavaScriptFundamentals/06.Functions/scripts/02.Reverse-number.js
@@ -1,47 +1,36 @@
 var pr2 = function () {
 
-    var arr1,
-        arr2,
-        counter = 0,
-        i,
-        min_arr_len;
-
-    arr1 = document.getElementById('problem21').value.split('');
-    arr2 = document.getElementById('problem22').value.split('');
-    min_arr_len = Math.min(arr1.length, arr2.length);
+    var txtInput = document.getElementById('problem2').value;
 
     jsConsole.writeLine('<br>=================== Problem 2 ===================');
-    jsConsole.writeLine('array 1: ' + arr1.join(', '));
-    jsConsole.writeLine('array 2: ' + arr2.join(', '));
-
-    for (i = 0; i < min_arr_len; i += 1) {
-        if (arr1[i] < arr2[i]) {
-            console.log('arr1 is lexicographically before arr2');
-            jsConsole.writeLine('arr1 is lexicographically before arr2');
-            break;
-        }
-        else if (arr1[i] > arr2[i]) {
-            console.log('arr1 is lexicographically after arr2');
-            jsConsole.writeLine('arr1 is lexicographically after arr2');
-            break;
-        }
-        else {
-            counter += 1;
-            if (counter === min_arr_len) {
-                if (arr1.length < arr2.length) {
-                    console.log('arr1 is lexicographically before arr2');
-                    jsConsole.writeLine('arr1 is lexicographically before arr2');
-                }
-                else if (arr1.length > arr2.length) {
-                    console.log('arr1 is lexicographically after arr2');
-                    jsConsole.writeLine('arr1 is lexicographically after arr2');
-                }
-                else {
-                    console.log('arr1 = arr2');
-                    jsConsole.writeLine('arr1 = arr2');
-                }
-            }
-        }
-    }
+    console.log('Input: ' + txtInput);
+    console.log('Reversed: ' + reverseNum(txtInput));
+    jsConsole.writeLine('Input: ' + txtInput);
+    jsConsole.writeLine('Reversed: ' + reverseNum(txtInput));
     jsConsole.writeLine('=================================================');
+}
+
+function reverseNum(input) {
+    var num_orig,
+        num_reversed = '',
+        arr,
+        len,
+        i;
+
+    if (!(isNaN(input))) {
+        num_orig = input;
+
+        arr = num_orig.split('');
+
+        len = arr.length;
+
+        for (i = len - 1; i >= 0; i -= 1) {
+            num_reversed += arr[i];
+        }
+        num_reversed *= 1;
+    }
+    else {
+        num_reversed = 'Not a number. Please input a number!';
+    }
+    return num_reversed;
 }
