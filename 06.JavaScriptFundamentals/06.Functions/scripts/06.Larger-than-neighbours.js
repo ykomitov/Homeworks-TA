ebc93eea3b7bcd8f8dfd31175e0f89c899b8c371
ykomitov <yavork@gmail.com>
2015-06-04 15:41:14 +0300

    HW 6 - OK

--- 06.JavaScriptFundamentals/06.Functions/scripts/06.Larger-than-neighbours.js
+++ 06.JavaScriptFundamentals/06.Functions/scripts/06.Larger-than-neighbours.js
@@ -1,38 +1,51 @@
 var pr6 = function () {
-    var arr,
-        counter_max = 1,
-        counter_temp = 1,
-        i,
-        j,
-        len,
-        index,
-        num;
-
-    arr = document.getElementById('problem6').value.split(',');
-
-    //convert all elements of the array to numbers
-    for (index in arr) {
-        arr[index] = arr[index] * 1;
-    }
-
-    //find the most frequent number
-    for (i = 0, len = arr.length; i < len - 1; i += 1) {
-        counter_temp = 1;
-        for (j = i + 1; j < len; j += 1) {
-            if (arr[i] === arr[j]) {
-                counter_temp += 1;
-            }
-        }
-        if (counter_max < counter_temp) {
-            counter_max = counter_temp;
-            num = arr[i];
-        }
-    }
-    console.log(arr);
-    console.log('Most frequent number: ' + num + ' (' + counter_max + ' times)');
+    var sampleArr = [1, 1, 3, 4, 1, 3, 4, 6, 3, 4, -1, 10, 11],
+        index = 3,
+        isLargerThanNeighbours = largerThanNeighbours(sampleArr, index);
 
     jsConsole.writeLine('<br>=================== Problem 6 ===================');
-    jsConsole.writeLine(arr);
-    jsConsole.writeLine('Most frequent number: ' + num + ' (' + counter_max + ' times)');
+    jsConsole.writeLine('Array: ' + sampleArr);
+    console.log(sampleArr);
+
+    if (isLargerThanNeighbours === 1) {
+        console.log(sampleArr[index] + ' at index ' + index + ' is larger than neighbours');
+        jsConsole.writeLine(sampleArr[index] + ' at index ' + index + ' is larger than neighbours');
+    }
+    else if (isLargerThanNeighbours === 0) {
+        console.log(sampleArr[index] + ' at index ' + index + ' is not larger than neighbours');
+        jsConsole.writeLine(sampleArr[index] + ' at index ' + index + ' is not larger than neighbours');
+    }
+    else {
+        console.log(isLargerThanNeighbours);
+        jsConsole.writeLine(isLargerThanNeighbours);
+    }
+
     jsConsole.writeLine('=================================================');
+}
+
+function largerThanNeighbours(arr, position) {
+    var len = arr.length,
+        isLarger;
+
+    if ((position > (len - 1)) || (position < 0)) {
+        isLarger = 'Index out or range!'
+        return isLarger;
+    }
+
+    if (position === (len - 1)) {
+        isLarger = arr[position] + ' at index ' + position + ' is the last element in the array';
+    }
+    else if (position === 0) {
+        isLarger = arr[position] + ' at index ' + position + ' is the first element in the array';
+    }
+    else {
+        if ((arr[position] > arr[position + 1]) && (arr[position] > arr[position - 1])) {
+            isLarger = 1;
+        }
+        else {
+            isLarger = 0;
+        }
+    }
+
+    return isLarger;
 }
--- 06.JavaScriptFundamentals/06.Functions/scripts/07.First-larger-than-neighbours.js
+++ 06.JavaScriptFundamentals/06.Functions/scripts/07.First-larger-than-neighbours.js
@@ -1,72 +1,29 @@
 var pr7 = function () {
-    var arr,
-        key,
-        i_min,
-        i_max,
-        i_mid,
-        len,
-        index;
 
-    arr = document.querySelector('input[name="pr7r"]:checked').value.split(','); //get the array from selected radio button
-    key = document.getElementById('problem7').value; //get the key from the text input
+    var arr = [1, 2, 3, 44, 55, 6, 6, 77, 8, 9],
+        firstLargerIndex = firstLarger(arr);
 
     jsConsole.writeLine('<br>=================== Problem 7 ===================');
-    //if key is present, convert to number
-    if (key) {
-        key *= 1;
-
-        //convert all elements of the array to numbers
-        for (index in arr) {
-            arr[index] = arr[index] * 1;
-        }
-
-        len = arr.length;
-        i_min = 0;
-        i_max = len - 1;
-
-        console.log('Original array:');
-        console.log(arr);
-        console.log('Key: ' + key);
-        jsConsole.writeLine('Input array: ' + arr);
-        jsConsole.writeLine('Key: ' + key);
-
-        //search for the key using binary search algorithm
-        while (i_max >= i_min) {
-
-            if (isNaN(key)) {
-                console.log('Search key is not a number!');
-                jsConsole.writeLine('Search key is not a number!');
-                break;
-            }
-
-            if (!(i_min) && !(i_max)) {
-                i_mid = 0;
-            }
-            else {
-                i_mid = (i_min + i_max) / 2 | 0;
-            }
-
-            if (arr[i_mid] === key) {
-                console.log('Key found at index ' + i_mid);
-                jsConsole.writeLine('Key found at index ' + i_mid);
-                break;
-            }
-            else if (arr[i_mid] < key) {
-                i_min = i_mid + 1;
-            }
-            else {
-                i_max = i_mid - 1;
-            }
-        }
-
-        if (i_max < i_min) {
-            console.log('Key not found');
-            jsConsole.writeLine('Key not found');
-        }
-    }
-    else {
-        console.log('Please enter search key!');
-        jsConsole.writeLine('Please enter search key!');
-    }
+    jsConsole.writeLine('Array: ' + arr);
+    jsConsole.writeLine('First larger than neighbours at index: ' + firstLargerIndex);
+    console.log(arr);
+    console.log('First larger than neighbours at index: ' + firstLargerIndex);
     jsConsole.writeLine('=================================================');
+}
+
+function firstLarger(array) {
+    var i,
+        len,
+        firstLarger = -1,
+        largerThanN;
+
+    for (i = 0, len = array.length; i < len; i += 1) {
+        largerThanN = largerThanNeighbours(array, i);
+
+        if (largerThanN === 1) {
+            firstLarger = i;
+            break;
+        }
+    }
+    return firstLarger;
 }
