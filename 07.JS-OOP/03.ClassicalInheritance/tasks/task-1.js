d201ea0fb3ebe41869aed71207c427d26452c514
ykomitov <yavork@gmail.com>
2015-06-19 22:18:46 +0300

    HW2 & 3 - OK

--- 07.JS-OOP/03.ClassicalInheritance/tasks/task-1.js
+++ 07.JS-OOP/03.ClassicalInheritance/tasks/task-1.js
@@ -19,44 +19,9 @@ function solve() {
     var Person = (function () {
         function Person(firstName, lastName, age) {
             validatePerson(firstName, lastName, age);
-
             this.firstname = firstName;
             this.lastname = lastName;
             this.age = age * 1;
-        }
-
-        Person.prototype.introduce = function () {
-            return 'Hello! My name is ' + this.firstname + ' ' + this.lastname + ' and I am ' + this.age + '-years-old';
-        };
-
-        Object.defineProperty(Person.prototype, 'fullname', {
-            get: function () {
-                return (this.firstname + ' ' + this.lastname);
-            },
-            set: function (fullname) {
-                validateChars(fullname);
-                var firstN = fullname.split(' ')[0];
-                var lastN = fullname.split(' ')[1];
-
-                if (firstN.length < 3 || firstN.length > 20 || lastN.length < 3 || lastN.length > 20) {
-                    throw new Error('FirstName & LastName must have length between 3 and 20 characters');
-                }
-
-                var fullName = fullname.split(' ');
-                this.firstname = fullName[0];
-                this.lastname = fullName[1];
-            }
-        });
-
-        function validateChars(name) {
-            var i, len, charCode;
-
-            for (i = 0, len = name.length; i < len; i += 1) {
-                charCode = name.charCodeAt(i);
-                if ((charCode < 65 && charCode > 90) && (charCode < 97 && charCode > 122)) {
-                    throw new Error('Names can only contain Latin lower & uppercase letters');
-                }
-            }
         }
 
         function validatePerson(fName, lName, age) {
@@ -81,8 +46,112 @@ function solve() {
             validateChars(lName);
         }
 
+        function validateChars(name) {
+            var i, len, charCode;
+
+            for (i = 0, len = name.length; i < len; i += 1) {
+                charCode = name.charCodeAt(i);
+                if ((charCode < 65 && charCode > 90) && (charCode < 97 && charCode > 122)) {
+                    throw new Error('Names can only contain Latin lower & uppercase letters');
+                }
+            }
+        }
+
         return Person;
     }());
+
+    Person.prototype = {
+        get fullname() {
+            return this.firstname + ' ' + this.lastname;
+        },
+        set fullname(fullname) {
+            var firstN = fullname.split(' ')[0];
+            var lastN = fullname.split(' ')[1];
+
+            if (firstN.length < 3 || firstN.length > 20 || lastN.length < 3 || lastN.length > 20) {
+                throw new Error('FirstName & LastName must have length between 3 and 20 characters');
+            }
+
+            var fullName = fullname.split(' ');
+            this.firstname = fullName[0];
+            this.lastname = fullName[1];
+        }
+    };
+
+    Person.prototype.introduce = function () {
+        return 'Hello! My name is ' + this.firstname + ' ' + this.lastname + ' and I am ' + this.age + '-years-old';
+    };
+
     return Person;
 }
 module.exports = solve;
+
+//function solve() {
+//    var Person = (function () {
+//        function Person(firstName, lastName, age) {
+//            this.validatePerson(firstName, lastName, age);
+//            this.firstname = firstName;
+//            this.lastname = lastName;
+//            this.age = age * 1;
+//        }
+//
+//        return Person;
+//    }());
+//
+//    Person.prototype.validatePerson = function(fName, lName, age) {
+//
+//        if (typeof(fName) !== 'string' || typeof(lName) !== 'string') {
+//            throw new Error('FirstName & LastName must always be strings');
+//        }
+//
+//        if (fName.length < 3 || fName.length > 20 || lName.length < 3 || lName.length > 20) {
+//            throw new Error('FirstName & LastName must have length between 3 and 20 characters');
+//        }
+//
+//        if (isNaN(age * 1)) {
+//            throw new Error('Age must be a number!');
+//        } else {
+//            if (age * 1 < 0 || age * 1 > 150) {
+//                throw new Error('Age must be between 0 and 150!');
+//            }
+//        }
+//
+//        this.validateChars(fName);
+//        this.validateChars(lName);
+//    };
+//
+//    Person.prototype.validateChars = function(name) {
+//        var i, len, charCode;
+//
+//        for (i = 0, len = name.length; i < len; i += 1) {
+//            charCode = name.charCodeAt(i);
+//            if ((charCode < 65 && charCode > 90) && (charCode < 97 && charCode > 122)) {
+//                throw new Error('Names can only contain Latin lower & uppercase letters');
+//            }
+//        }
+//    };
+//
+//    Person.prototype.introduce = function () {
+//        return 'Hello! My name is ' + this.firstname + ' ' + this.lastname + ' and I am ' + this.age + '-years-old';
+//    };
+//
+//    Object.defineProperty(Person.prototype, 'fullname', {
+//        get: function () {
+//            return (this.firstname + ' ' + this.lastname);
+//        },
+//        set: function (fullname) {
+//            this.validateChars(fullname);
+//            var firstN = fullname.split(' ')[0];
+//            var lastN = fullname.split(' ')[1];
+//
+//            if (firstN.length < 3 || firstN.length > 20 || lastN.length < 3 || lastN.length > 20) {
+//                throw new Error('FirstName & LastName must have length between 3 and 20 characters');
+//            }
+//
+//            var fullName = fullname.split(' ');
+//            this.firstname = fullName[0];
+//            this.lastname = fullName[1];
+//        }
+//    });
+//    return Person;
+//}
