bd9e0d4e2837ee76433a963920269a258b6bef96
ykomitov <yavork@gmail.com>
2015-08-02 19:42:32 +0300

    HW2 - OK

--- 09.JS-UI-DOM/02.Event-Model/tasks/task-1.js
+++ 09.JS-UI-DOM/02.Event-Model/tasks/task-1.js
@@ -2,14 +2,69 @@
 
 /* 
 
-Create a function that takes an id or DOM element and:
-  
+ Create a function that takes an id or DOM element and:
 
-*/
 
-function solve(){
-  return function (selector) {
-  };
-};
+ */
+
+function solve() {
+    return function (selector) {
+        var selectedByID,
+            isDOMElement = checkIfObjectIsDOMElement(selector);
+
+        //Validation starts
+
+        if (!isDOMElement && typeof selector !== 'string') {
+            throw new Error('The parameter is neither a string, nor a DOM element!');
+        }
+
+        if (!isDOMElement) {
+            selectedByID = document.getElementById(selector);
+            var childNodesInSelection = selectedByID.children;
+
+            if (selectedByID === 'undefined' || selectedByID === null) {
+                throw new Error('The provided ID does not select anything!');
+            }
+        }
+
+        if (isDOMElement && !document.contains(selector)) {
+            throw new Error('The provided DOM element is non-existent');
+        }
+
+        //Validation ends
+
+        var elementsWithButtonClass = selectedByID.getElementsByClassName('button');
+
+        var i, len = elementsWithButtonClass.length;
+        for (i = 0; i < len; i += 1) {
+            elementsWithButtonClass[i].innerHTML = 'hide';
+            elementsWithButtonClass[i].addEventListener('click', clickEv, false);
+        }
+
+        function clickEv(ev) {
+
+            var clickedNode = ev.target;
+            var nextElement = clickedNode.nextElementSibling;
+
+            while (nextElement.className !== 'content') {
+                nextElement = nextElement.nextElementSibling;
+            }
+
+            if (nextElement.style.display !== 'none') {
+                nextElement.style.display = 'none';
+                clickedNode.innerHTML = 'show'
+            } else {
+                nextElement.style.display = '';
+                clickedNode.innerHTML = 'hide'
+            }
+        }
+
+        function checkIfObjectIsDOMElement(object) {
+            if (object.tagName) {
+                return true;
+            }
+        }
+    };
+}
 
 module.exports = solve;
