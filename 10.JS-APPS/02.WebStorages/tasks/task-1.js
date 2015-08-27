56e8bfe435a2b0b47fe92ab10005bab953e3179c
ykomitov <yavork@gmail.com>
2015-08-28 00:23:43 +0300

    HW2 - PR1 - partially solved

--- 10.JS-APPS/02.WebStorages/tasks/task-1.js
+++ 10.JS-APPS/02.WebStorages/tasks/task-1.js
@@ -1,21 +1,107 @@
 function solve() {
 
-  function init(playerName, endCallback) {
+    function init(playerName, endCallback) {
+        this.playerName = playerName;
+        this.numberAsString = generateNumber();
 
-  }
+        localStorage.setItem('playerName', this.playerName);
+        localStorage.setItem('number', this.numberAsString);
+        localStorage.setItem('numberOfGuesses', 0);
 
-  function guess(number) {
+        return this;
+    }
 
-  }
+    function guess(number) {
+        var i,
+            sheepCount = 0,
+            ramCount = 0,
+            numberAsArr = number.toString().split(''),
+            originalNumAsStr = localStorage.getItem('number'),
+            originalNumAsArr = originalNumAsStr.split(''),
+            result = {sheep: 0, rams: 0},
+            currentHighScore = JSON.parse(localStorage.getItem('highScore')) || {},
+            currentNumberOfGuesses = localStorage.getItem('numberOfGuesses');
 
-  function getHighScore(count) {
+        // Check for exact match & update high score
+        if (number == originalNumAsStr) {
+            currentNumberOfGuesses++;
+            var player = localStorage.getItem('playerName');
+            currentHighScore[player] = currentNumberOfGuesses;
 
-  }
+            //TODO: check if player name exists and if current score is better than player highscore
+            localStorage.setItem('highScore', JSON.stringify(currentHighScore));
+            console.log('success');
+            return;
+        }
 
-  return {
-    init, guess, getHighScore
-  }
+        // Initial check for 'rams'
+        for (i = 0; i < 4; i += 1) {
+            if (numberAsArr[i] === originalNumAsArr[i]) {
+                ramCount++;
+                originalNumAsArr[i] = 'x';
+            }
+
+            originalNumAsStr = originalNumAsArr.join('');
+        }
+
+        // Check for 'sheep'
+        for (i = 0; i < 4; i += 1) {
+            var positionInOriginalNumber = originalNumAsStr.indexOf(numberAsArr[i]);
+
+            if (positionInOriginalNumber < 0) {
+                continue;
+            } else {
+                sheepCount++;
+                originalNumAsArr[positionInOriginalNumber] = 'x';
+            }
+
+            originalNumAsStr = originalNumAsArr.join('');
+        }
+
+        result.sheep = sheepCount;
+        result.rams = ramCount;
+        currentNumberOfGuesses++;
+        localStorage.setItem('numberOfGuesses', currentNumberOfGuesses);
+
+        return result;
+    }
+
+    function getHighScore(count) {
+        var sortable = [];
+        var highScore = JSON.parse(localStorage.getItem('highScore'));
+
+        for (var player in highScore) {
+            sortable.push([player, highScore[player]]);
+        }
+
+        sortable.sort(function (a, b) {
+            return a[1] - b[1]
+        });
+
+        for (var i = 0; i < sortable.length; i += 1) {
+            console.log(i + 1 + ' - ' + sortable[i][0] + ': ' + sortable[i][1]);
+        }
+    }
+
+    function generateNumber() {
+        var a = getRandomInt(1, 9),
+            b = getRandomInt(0, 9),
+            c = getRandomInt(0, 9),
+            d = getRandomInt(0, 9);
+
+        var numberAsString = '' + a + b + c + d;
+
+        return numberAsString;
+
+        function getRandomInt(min, max) {
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
+    }
+
+    return {
+        init, guess, getHighScore
+    }
 }
 
 
-module.exports = solve;
+//module.exports = solve;
