6312409bd6e9578110762b279df38b583ea133b5
ykomitov <yavork@gmail.com>
2015-10-02 08:41:41 +0300

    HW 2 - 7 OK, skeleton for 8 added

---
+++ 11.Databases/02.ProcessingXMLin.NET/07.CreateXmlDocument/CreateXmlDocFromFile.cs
@@ -0,0 +1,49 @@
+﻿namespace Processing.Xml
+{
+    using System;
+    using System.Text;
+    using System.Xml;
+
+    public class CreateXmlFromFile
+    {
+        public static void Main()
+        {
+            string line;
+            string fileName = "../../sample.xml";
+            Encoding encoding = Encoding.GetEncoding("windows-1251");
+            string[] tags = { "name", "address", "phone" };
+
+            using (System.IO.StreamReader reader = new System.IO.StreamReader("../../sample.txt"))
+            {
+                using (XmlTextWriter writer = new XmlTextWriter(fileName, encoding))
+                {
+                    writer.Formatting = Formatting.Indented;
+                    writer.IndentChar = '\t';
+                    writer.Indentation = 1;
+
+                    writer.WriteStartDocument();
+                    writer.WriteStartElement("persons");
+                    writer.WriteAttributeString("name", "Person List");
+
+
+                    // Read the input file line by line.
+                    for (int i = 0; i < tags.Length; i++)
+                    {
+                        line = reader.ReadLine();
+                        writer.WriteStartElement(tags[i]);
+                        writer.WriteValue(line);
+                        writer.WriteEndElement();
+
+                        Console.WriteLine(line);
+                    }
+                    writer.WriteEndElement();
+                }
+            }
+
+            Console.WriteLine("Document {0} created.", fileName);
+
+            // Suspend the screen.
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 11.Databases/02.ProcessingXMLin.NET/08.CreateXmlDocWithAlbums/CreateAlbumsXml.cs
@@ -0,0 +1,16 @@
+﻿namespace Processing.Xml
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class CreateAlbumsXml
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
