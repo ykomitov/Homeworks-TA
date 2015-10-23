9e2eca350f885930b68e0133251c6bd119d4106b
ykomitov <yavork@gmail.com>
2015-10-24 00:13:03 +0300

    HW13 - OK

---
+++ 11.Databases/13.DbPerformance/DatabasePerformance.Data.Importer/RandomDataGenerator.cs
@@ -0,0 +1,38 @@
+﻿namespace DatabasePerformance.Data.Importer
+{
+    using System;
+    using System.Linq;
+
+    public class RandomDataGenerator
+    {
+        private const string Letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        private Random random;
+
+        public RandomDataGenerator()
+        {
+            this.random = new Random();
+        }
+
+        public int GetRandomNumber(int min, int max)
+        {
+            return this.random.Next(min, max + 1);
+        }
+
+        public string GetRandomString(int length)
+        {
+            var result = new char[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                result[i] = Letters[this.GetRandomNumber(0, Letters.Length - 1)];
+            }
+
+            return new string(result);
+        }
+
+        public string GetRandomStringWithRandomLength(int min, int max)
+        {
+            return this.GetRandomString(this.GetRandomNumber(min, max));
+        }
+    }
+}
---
+++ 11.Databases/13.DbPerformance/DatabasePerformance.TestApp/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace DatabasePerformance.TestApp
+{
+    using System;
+    using System.Linq;
+    using DatabasePerformance.Data;
+    using DatabasePerformance.Data.Importer;
+
+    class Program
+    {
+        static void Main()
+        {
+            var db = new DatabasePerformanceEntities();
+
+            var random = new RandomDataGenerator();
+
+            Console.WriteLine("Seeding data. Be patient!");
+            for (int i = 0; i < 10000000; i++)
+            {
+                var log = new Log
+                {
+                    text = random.GetRandomStringWithRandomLength(10, 50),
+                    date = new DateTime(
+                        random.GetRandomNumber(2010, 2015),
+                        random.GetRandomNumber(1, 12),
+                        random.GetRandomNumber(1, 28),
+                        random.GetRandomNumber(1, 12),
+                        random.GetRandomNumber(1, 59),
+                        random.GetRandomNumber(1, 59)
+                        )
+                };
+
+                db.Logs.Add(log);
+
+                if (i % 100 == 0)
+                {
+                    Console.Write("=");
+                    db.SaveChanges();
+                    db.Dispose();
+                    db = new DatabasePerformanceEntities();
+                }
+            }
+
+            db.SaveChanges();
+
+            Console.WriteLine("Man, you are patient... Congratulations!");
+        }
+    }
+}
