b90be26b3a990d27beeedae11d7e5559a6de8243
ykomitov <yavork@gmail.com>
2015-11-04 08:44:29 +0200

    Added Jedi Meditation problem

---
+++ 12.DSA/99.JediMeditation/JediMeditation/Startup.cs
@@ -0,0 +1,55 @@
+﻿namespace JediMeditation
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var jedi = Console.ReadLine();
+            jedi.Trim();
+            var jediArr = jedi.Split(' ');
+
+            var jediMaster = new Queue<string>();
+            var jediKnight = new Queue<string>();
+            var jediPadawan = new Queue<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var currentJedi = jediArr[i];
+
+                switch (currentJedi[0])
+                {
+                    case 'm': jediMaster.Enqueue(currentJedi); break;
+                    case 'k': jediKnight.Enqueue(currentJedi); break;
+                    case 'p': jediPadawan.Enqueue(currentJedi); break;
+                    default: break;
+                }
+            }
+
+            var sb = new StringBuilder();
+            foreach (var jed in jediMaster)
+            {
+                sb.Append(jed);
+                sb.Append(" ");
+            }
+
+            foreach (var jed in jediKnight)
+            {
+                sb.Append(jed);
+                sb.Append(" ");
+            }
+
+            foreach (var jed in jediPadawan)
+            {
+                sb.Append(jed);
+                sb.Append(" ");
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
