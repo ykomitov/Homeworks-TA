32544e11ef95516437c62a0cddc73f6fb785e57a
ykomitov <yavork@gmail.com>
2015-11-09 20:45:19 +0200

    HW5 - done, added trie implementation in the last problem

---
+++ 12.DSA/05.OtherDataStructures/03.Trie/Datareader.cs
@@ -0,0 +1,40 @@
+﻿namespace _03.Trie
+{
+    using System.IO;
+
+    public class DataReader
+    {
+        private const int LoopCount = 1;
+        private TrieNode trie;
+        private string path;
+
+        public DataReader(string path, ref TrieNode root)
+        {
+            this.trie = root;
+            this.path = path;
+        }
+
+        public void ThreadRun()
+        {
+            // Can fake large data set buy parsing smaller file multiple times
+            for (int i = 0; i < LoopCount; i++) 
+            {
+                using (FileStream fstream = new FileStream(this.path, FileMode.Open, FileAccess.Read))
+                {
+                    using (StreamReader sreader = new StreamReader(fstream))
+                    {
+                        string line;
+                        while ((line = sreader.ReadLine()) != null)
+                        {
+                            string[] chunks = line.Split(null);
+                            foreach (string chunk in chunks)
+                            {
+                                this.trie.AddWord(chunk.Trim());
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
