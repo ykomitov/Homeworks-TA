a888716ae973f50efe88710f081b9bb5985d5deb
ykomitov <yavork@gmail.com>
2016-02-17 00:26:30 +0200

    Added hw4 for ASP.NET MVC

---
+++ 17.ASP.NET-MVC/04.Caching/Caching/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Caching.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        [OutputCache(Duration = 120)]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string inputTown)
+        {
+            FakeDb.towns.Add(inputTown);
+            return this.Redirect("~/");
+        }
+
+        [OutputCache(Duration = 60 * 60)]
+        public ActionResult About()
+        {
+            //ViewBag.Message = "Your application description page.";
+            ViewBag.Message = DateTime.Now;
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ 17.ASP.NET-MVC/04.Caching/Caching/FakeDb.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Caching
+{
+    public static class FakeDb
+    {
+        public static List<string> towns = new List<string> { "Kaspichan", "Lukovit" };
+    }
+}
